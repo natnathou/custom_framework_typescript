2739a12b9c1e09c3f5f57db78685682ab60cd023
natnathou <ne.zaffran@gmail.com>
2020-08-09 19:12:17 +0300

    update

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,34 @@
-import { UserEdit } from './views/UserEdit';
-import { User } from './models/User';
+// import { UserEdit } from './views/UserEdit';
+// import { User } from './models/User';
 
-const user = User.buildUser({ name: 'Vinc', age: 17 });
+// const user = User.buildUser({ name: 'Vinc', age: 17 });
 
-const root = document.getElementById('root');
-if (root) {
-	const userEdit = new UserEdit(root, user);
-	userEdit.render();
-	console.log(userEdit);
-} else {
-	throw new Error('Root element not found');
-}
+// const root = document.getElementById('root');
+// if (root) {
+// 	const userEdit = new UserEdit(root, user);
+// 	userEdit.render();
+// 	console.log(userEdit);
+// } else {
+// 	throw new Error('Root element not found');
+// }
+
+import { UserList } from './views/UserList';
+import { Collection } from './models/Collection';
+import { UserProps, User } from './models/User';
+
+const users = new Collection(
+	'http://localhost:3000/users',
+	(json: UserProps) => {
+		return User.buildUser(json);
+	}
+);
+
+users.on('change', () => {
+	const root = document.getElementById('root');
+
+	if (root) {
+		new UserList(root, users).render();
+	}
+});
+
+users.fetch();
